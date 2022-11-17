5b59a0fb1148d75d09e3c42116a8ccae67570ff2
AkashMVinoj <akashmvinoj30@gmail.com>
2022-11-17 11:57:51 +0530

    cprogram

---
+++ increment.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+int a,b;
+printf("\n enter two numbers");
+scanf("%d%d",&a,&b);
+if (a>=b){
+   if(a==b)
+{
+printf("\n a is equal to b");
+
+}
+else
+{
+  printf("\n a is greater than b");
+}
+
+}else
+printf("\n b is greater than a");
+
+
+
+}
+
+
+
---
+++ loop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a;
+for(a=10;a>=1;a--)
+{
+printf("%d\n",a);
+}}
+
